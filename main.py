81475821edb2665a0419ae899c908179d089dee2
Ruben Leiva <rubenlrd@gmail.com>
2025-06-03 17:15:22 -0300

    se coorigio error de ortografia en el archivo main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from os import path
 from principal.dataset_csv import DatasetCSV
 
-file_name = input("Ingres nombre del archivo: ")
+file_name = input("Ingrese nombre del archivo: ")
 # Ruta del la ubicación del archivo csv
 csv_path = path.join(path.dirname(__file__), "archivos", file_name)
 
